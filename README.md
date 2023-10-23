c67de09ed2c4adb5bd2a12604677d9d0bd1f7355
Dave Lewis <dave@dllewis.org>
2023-10-23 21:17:09 +0100

    Start docs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # actix-htmx
 
-Middleware to make working with [htmx](https://htmx.org) and [Actix Web](https://github.com/actix/actix-web) easier.
+> Middleware to make working with [htmx](https://htmx.org) and [Actix Web](https://github.com/actix/actix-web) easier.
+
+[![crates.io](https://img.shields.io/crates/v/actix-htmx?label=latest)](https://crates.io/crates/actix-htmx)
+![Apache 2.0 or MIT licensed](https://img.shields.io/crates/l/actix-htmx)
+
+`actix-htmx` allows you to easily interact with [htmx](https://htmx.org) request and response headers from within an [Actix Web](https://github.com/actix/actix-web) application.
--- actix-htmx/src/lib.rs
+++ actix-htmx/src/lib.rs
@@ -1,3 +1,47 @@
+//! htmx middleware for Actix Web.
+//!
+//! `actix-htmx` provides a method of easily working with htmx in actix web applications.
+//! Wrap services with [`HtmxMiddleware`] to enable htmx support, and access the [`HtmxDetails`]
+//! extractor in your handlers to get information about the current htmx state. Helper methods also
+//! exist to enable you to set htmx response headers, allowing easy triggering of htmx events from
+//! server side code.
+//!
+//! # Getting Started
+//! Register [`HtmxMiddleware`] on your `App`:
+//!
+//! ```no_run
+//! use actix_htmx::{HtmxDetails, HtmxMiddleware, TriggerType};
+//! use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+//!
+//! #[actix_web::main]
+//! async fn main() -> std::io::Result<()> {
+//!     HttpServer::new(|| {
+//!         App::new()
+//!             .wrap(HtmxMiddleware)
+//!             .service(web::resource("/").to(index))
+//!     })
+//!     .bind("0.0.0.0:8080")?
+//!     .run()
+//!     .await
+//! }
+//!
+//! async fn index(htmx_details: HtmxDetails) -> impl Responder {
+//!     if htmx_details.is_htmx {
+//!         // build a partial view
+//!     } else {
+//!         // build a full view
+//!     }
+//!     htmx_details.trigger_event(
+//!         "my_event".to_string(),
+//!         r#"{"level": "info", "message": "my event message!"}"#.to_string(),
+//!         TriggerType::Standard
+//!     );
+//!
+//!     HttpResponse::Ok().content_type("text/html").body(// render the view)
+//!
+//! }
+//! ```
+
 mod headers;
 mod htmx;
 mod middleware;
