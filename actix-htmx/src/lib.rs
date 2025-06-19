9f5ac298d91d3e6700522c1fff903434483e4987
Dave Lewis <dave@dllewis.org>
2025-06-19 22:44:28 +0100

    Added basic tests

--- actix-htmx/src/lib.rs
+++ actix-htmx/src/lib.rs
@@ -37,7 +37,7 @@
 //!         Some(TriggerType::Standard)
 //!     );
 //!
-//!     HttpResponse::Ok().content_type("text/html").body(// render the view)
+//!     HttpResponse::Ok().content_type("text/html").body("<html><head>Test!</head><body>My Content</body></html>")
 //!
 //! }
 //! ```
@@ -50,3 +50,137 @@ pub use self::{
     htmx::{Htmx, SwapType, TriggerType},
     middleware::HtmxMiddleware,
 };
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::headers::ResponseHeaders;
+    use actix_web::{
+        http::header::HeaderName,
+        test::{self, TestRequest},
+        web, App, HttpResponse,
+    };
+
+    #[actix_web::test]
+    async fn test_htmx_middleware_basic() {
+        let app = test::init_service(App::new().wrap(HtmxMiddleware).route(
+            "/test",
+            web::get().to(|htmx: Htmx| async move {
+                htmx.trigger_event(
+                    "test-event".to_string(),
+                    Some("test-value".to_string()),
+                    Some(TriggerType::Standard),
+                );
+                HttpResponse::Ok().finish()
+            }),
+        ))
+        .await;
+
+        let req = TestRequest::get()
+            .uri("/test")
+            .insert_header((HeaderName::from_static("hx-request"), "true"))
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+
+        let trigger_header = resp
+            .headers()
+            .get(HeaderName::from_static(ResponseHeaders::HX_TRIGGER))
+            .unwrap();
+        assert!(trigger_header
+            .to_str()
+            .unwrap()
+            .contains(r#""test-event": "test-value""#));
+    }
+
+    #[actix_web::test]
+    async fn test_htmx_middleware_after_settle() {
+        let app = test::init_service(App::new().wrap(HtmxMiddleware).route(
+            "/test",
+            web::get().to(|htmx: Htmx| async move {
+                htmx.trigger_event(
+                    "settle-event".to_string(),
+                    None,
+                    Some(TriggerType::AfterSettle),
+                );
+                HttpResponse::Ok().finish()
+            }),
+        ))
+        .await;
+
+        let req = TestRequest::get()
+            .uri("/test")
+            .insert_header((HeaderName::from_static("hx-request"), "true"))
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+
+        let settle_header = resp
+            .headers()
+            .get(HeaderName::from_static(ResponseHeaders::HX_TRIGGER_AFTER_SETTLE))
+            .unwrap();
+        
+        assert!(settle_header
+            .to_str()
+            .unwrap()
+            .contains("settle-event"));
+    }
+
+    #[actix_web::test]
+    async fn test_htmx_request_detection() {
+        let app = test::init_service(App::new().wrap(HtmxMiddleware).route(
+            "/test",
+            web::get().to(|htmx: Htmx| async move {
+                assert!(htmx.is_htmx);
+                HttpResponse::Ok().finish()
+            }),
+        ))
+        .await;
+
+        let req = TestRequest::get()
+            .uri("/test")
+            .insert_header((HeaderName::from_static("hx-request"), "true"))
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_web::test]
+    async fn test_non_htmx_request() {
+        let app = test::init_service(App::new().wrap(HtmxMiddleware).route(
+            "/test",
+            web::get().to(|htmx: Htmx| async move {
+                assert!(!htmx.is_htmx);
+                HttpResponse::Ok().finish()
+            }),
+        ))
+        .await;
+
+        let req = TestRequest::get().uri("/test").to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_web::test]
+    async fn test_boosted_request() {
+        let app = test::init_service(App::new().wrap(HtmxMiddleware).route(
+            "/test",
+            web::get().to(|htmx: Htmx| async move {
+                assert!(htmx.boosted);
+                HttpResponse::Ok().finish()
+            }),
+        ))
+        .await;
+
+        let req = TestRequest::get()
+            .uri("/test")
+            .insert_header((HeaderName::from_static("hx-boosted"), "true"))
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+}
